9ca07832c849b27c63031877b1f0dc009f4a9f7b
aeryli <72891343+aeryli@users.noreply.github.com>
2026-09-01 21:42:17 -0700

    Update oggstream2.py

--- oggstream2.py
+++ oggstream2.py
@@ -1,38 +1,39 @@
 import http
 import time
+import socket
+import requests
 import webbrowser
 from pathlib import Path as pathy
 from librespot.core import Session
 from librespot.metadata import TrackId
+from spotify_scraper import SpotifyClient
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from librespot.audio.decoders import AudioQuality, VorbisOnlyAudioQuality
 
+def gimmeIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('8.8.8.8', 1))
+        ip = s.getsockname()[0]
+    except Exception: ip = '127.0.0.1'
+    finally: s.close()
+    return ip
 class mergle:
-    def __init__(self):
-        self.track_uri = "spotify:track:2FpEXjGqe2dJJ9oB8c8Io2"
-        if (self.track_uri == ""):
-            self.track_uri = input("Set the track uri(spotify:track:[trackID]): ")
-    def auth_url_callback(url):
-        webbrowser.open(url)
+    def __init__(self):          self.track_uri = "spotify:track:2FpEXjGqe2dJJ9oB8c8Io2"
+    def auth_url_callback(url):  webbrowser.open(url)
     def login(self):
         success_page = "<html><body><h1>Login Successful.</h1><p>You can close this window now.</p><script>setTimeout(() => {window.close()}, 100);</script></body></html>"
         self.session = Session.Builder().oauth(self.auth_url_callback, success_page).create()
     def getStrem(self, trackURI):
         track_id = TrackId.from_uri(trackURI)
-        content_stream = self.session.content_feeder().load(track_id, VorbisOnlyAudioQuality(AudioQuality.HIGH), False, None)
+        content_stream = self.session.content_feeder().load(track_id, VorbisOnlyAudioQuality(AudioQuality.NORMAL), False, None)
         self.raw_stream = content_stream.input_stream.stream()
     def verbose():
         print(self.raw_stream)
         print(type(self.raw_stream))
-    def strem(strem):
-        #wawa to 3ds / other thing (meaning stream to self)
-        while True:
-            chunk = self.raw_stream.read(4096)
-            if not chunk:
-                break
-            #streeeeeeeeeeeeeeeeeeeeeeeeeeeeeemmmmmmmmmmmmm
-    def stremchunk(self):
-        chunk = self.raw_stream.read(4096)
+    def stremchunk(self, chsize):
+        
+        chunk = self.raw_stream.read(chsize)
         return chunk
     def headers(write, mime):
         if mime:
@@ -44,7 +45,8 @@ class mergle:
 class MI(BaseHTTPRequestHandler):
     def do_GET(self):
         if (self.path.startswith("/music/")):
-            track_uri = self.path.split("/music/")[1].split("/")[0]
+            stuffs = self.path.split("/music/")[1].split("/")
+            track_uri = stuffs[0]
             if track_uri:
                 print(track_uri)
                 mergel = mergle()
@@ -55,30 +57,59 @@ class MI(BaseHTTPRequestHandler):
                 self.send_header("Content-Type", "audio/ogg")
                 self.send_header("Cache-Control", "no-cache")
                 self.end_headers()
-                
-                while True:
-                    chunk = mergel.stremchunk()
-                    if not chunk:
-                        break
-                    self.wfile.write(chunk)
-                    self.wfile.flush()
-                
-        elif (self.path.startswith("/source/")):
+                try:
+                    while True:
+                        chunk = mergel.stremchunk(1024*4)
+                        if not chunk:
+                            break
+                        self.wfile.write(chunk)
+                        self.wfile.flush()
+                except Exception as e:
+                    print(e)
+            
+        elif (self.path.startswith("/cover/")):
+            stuffs = self.path.split("/cover/")[1].split("/")[0].split("spotify:track:")[1]
+            
+            with SpotifyClient() as client:
+                track = client.get_track(stuffs)
+                response = requests.get(track.to_dict()["images"][0]["url"])
+            
+            self.send_response(200)
+            self.send_header("Content-Type", response.headers.get('Content-Type'))
+            self.send_header("Cache-Control", "no-cache")
+            self.end_headers()
+            self.wfile.write(response.content)
+            self.wfile.flush()
+            
+        elif (self.path.startswith("/data/")):
+            stuffs = self.path.split("/data/")[1].split("/")
+            with SpotifyClient() as client:
+                track = client.get_track(stuffs[0].split("spotify:track:")[1]).to_dict()
+            
+            pathys = stuffs[1:(len(stuffs)-1)]
+            for i in range(len(pathys)):
+                track = track[pathys[i]]
             self.send_response(200)
             self.send_header("Content-Type", "text/plain; charset=utf-8")
             self.send_header("Cache-Control", "no-cache")
             self.end_headers()
             
-            try:
-                chunk = open(r"C:\Users\alixc\3ds\spotify\spotify.c", "r", encoding="utf-8")
-                print(chunk.read())
-                self.wfile.write(chunk.read().encode())
-                self.wfile.flush()
-            except Exception as e:
-                print("Error: " + str(e))
+            self.wfile.write(str(track).encode())
+            self.wfile.flush()
         else:
             self.send_error(404)
     
 print(f"HTTP is here: {hasattr(http, 'HTTPStatus')}")
-httpd = HTTPServer(('', 8000), MI)
-httpd.serve_forever()
+print(f"Connect to: http://{gimmeIP()}:8000/music/spotify:track:xxxxxxxxxxxxxxxxxxxxxx")
+print(f"Example: http://{gimmeIP()}:8000/music/spotify:track:2FpEXjGqe2dJJ9oB8c8Io2")
+print(f"Example: http://{gimmeIP()}:8000/cover/spotify:track:2FpEXjGqe2dJJ9oB8c8Io2")
+print(f"Example: http://{gimmeIP()}:8000/data/spotify:track:2FpEXjGqe2dJJ9oB8c8Io2")
+try:
+    httpd = HTTPServer(('', 8000), MI)
+    httpd.serve_forever()
+except Exception as e:
+    print(e)
+
+
+
+
